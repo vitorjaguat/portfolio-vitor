bd5eae7a6fbb15f0733873164846d5c11c975173
Vitor Butkus <vitorbutkus@gmail.com>
2023-05-03 19:14:50 -0300

    change w-screen to w-full in index

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ import Contact from '@/components/Contact';
 
 export default function Home() {
   return (
-    <div className='w-screen overflow-x-hidden'>
+    <div className='w-full overflow-x-hidden'>
       <Main />
       <About />
       <Skills />
