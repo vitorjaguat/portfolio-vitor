11069fe112703a775671df975de6b8e1db76e257
Vitor Butkus <vitorbutkus@gmail.com>
2023-05-03 19:18:48 -0300

    add overflow-x-hidden w-screen in document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang='en'>
       <Head />
-      <body>
+      <body className='w-screen overflow-x-hidden'>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
